4c7c0c05f9e559c43603d311aea813e0898a7dde
Arthur Yefimov <arturefimov@gmail.com>
2021-04-18 01:36:53 +0300

    Make on Windows automation

--- make.bat
+++ make.bat
@@ -1,6 +1,26 @@
 @SET PROG=foc
 @SET runc=BlackBox\BlackBoxInterp.exe /USE .
 
+@IF NOT EXIST __WIN_BlackBox GOTO ENDIF1
+	@MOVE BlackBox __LIN_BlackBox
+	@MOVE __WIN_BlackBox BlackBox
+:ENDIF1
+
+@IF NOT EXIST Host\Mod\__WIN_Args.Mod GOTO ENDIF2
+	@MOVE Host\Mod\Args.Mod Host\Mod\__LIN_Args.Mod
+	@MOVE Host\Mod\__WIN_Args.Mod Host\Mod\Args.Mod
+:ENDIF2
+
+@IF NOT EXIST Host\Mod\__WIN_Files.Mod GOTO ENDIF3
+	@MOVE Host\Mod\Files.Mod Host\Mod\__LIN_Files.Mod
+	@MOVE Host\Mod\__WIN_Files.Mod Host\Mod\Files.Mod
+:ENDIF3
+
+@IF NOT EXIST Freeoberon\Mod\__WIN_LinkerProxy.Mod GOTO ENDIF4
+	@MOVE Freeoberon\Mod\LinkerProxy.Mod Freeoberon\Mod\__LIN_LinkerProxy.Mod
+	@MOVE Freeoberon\Mod\__WIN_LinkerProxy.Mod Freeoberon\Mod\LinkerProxy.Mod
+:ENDIF4
+
 @echo ConsCompiler.Compile('Host\Mod', 'Files.Mod') | %runc%
 @echo ConsCompiler.Compile('System\Mod', 'Args.Mod') | %runc%
 @echo ConsCompiler.Compile('Host\Mod', 'Args.Mod') | %runc%
